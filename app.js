59dde0368017c6ba1f2da30ee0a78dc3efcec5f1
Deepanshu40 <144712703+Deepanshu40@users.noreply.github.com>
2024-04-09 23:29:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ const app = express();
 dotenv.config({path:'./config/config.env'});
 
 
-app.use(cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
-    credentials:true,
-}))
+// app.use(cors({
+//     origin: [process.env.FRONTEND_URL],
+//     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
+//     credentials:true,
+// }))
 
 app.use(cookieParser());
 app.use(express.json());
@@ -28,7 +28,7 @@ app.use(fileUpload({
     tempFileDir : '/tmp/'
 }));
 
-// app.use(cors())
+app.use(cors())
  
 
 // app.use('/', (req, res) => {
