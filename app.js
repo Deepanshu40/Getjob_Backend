f112ea45dfbb6dad261d5244a308812a5e021791
Deepanshu40 <144712703+Deepanshu40@users.noreply.github.com>
2024-04-09 07:56:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ dotenv.config({path:'./config/config.env'});
 app.use(cors({
     origin: [process.env.FRONTEND_URL],
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
-    credentials:true,
-    preflightContinue: false,
+    credentials:true
 }))
 
 app.use(cookieParser());
